5840b764e50e93f0b3aeb5e7611ff3e32b3c6a45
Michal <michalkuchnickiisc@gmail.com>
2023-11-13 09:07:36 +0100

    Added input system

---
+++ Holt/src/Holt/Core/MouseButtonCodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// From glfw3.h
+#define HL_MOUSE_BUTTON_1         0
+#define HL_MOUSE_BUTTON_2         1
+#define HL_MOUSE_BUTTON_3         2
+#define HL_MOUSE_BUTTON_4         3
+#define HL_MOUSE_BUTTON_5         4
+#define HL_MOUSE_BUTTON_6         5
+#define HL_MOUSE_BUTTON_7         6
+#define HL_MOUSE_BUTTON_8         7
+#define HL_MOUSE_BUTTON_LAST      HL_MOUSE_BUTTON_8
+#define HL_MOUSE_BUTTON_LEFT      HL_MOUSE_BUTTON_1
+#define HL_MOUSE_BUTTON_RIGHT     HL_MOUSE_BUTTON_2
+#define HL_MOUSE_BUTTON_MIDDLE    HL_MOUSE_BUTTON_3
